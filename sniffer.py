3e7e91f1d9d4a13946a4fe30f21424284185af74
Alex Nguyen <alexnguyen7369@gmail.com>
2026-04-22 23:33:50 -0700

    cleaned code, store and used variable distribution

--- sniffer.py
+++ sniffer.py
@@ -9,21 +9,19 @@ def IP_sniffer(pkt):
 
         pkt_SRC_IP = pkt[IP].src
         pkt_DST_IP = pkt[IP].dst
+        pkt_proto = pkt.sprintf("%IP.proto%")
 
         print(f"Packet ID: {pkt[IP].id}")
         print(f"Source IP: {pkt_SRC_IP}")
         print(f"Destination IP: {pkt_DST_IP}")
+        print(f"Protocol: {pkt_proto}")
 
         if TCP in pkt:
-            print(pkt.sprintf("%IP.proto%"))
             print(f"Source Port: {pkt[TCP].sport}")
             print(f"Destination Port: {pkt[TCP].dport}")
         elif UDP in pkt:
-            print(pkt.sprintf("%IP.proto%"))
             print(f"Source Port: {pkt[UDP].sport}")
             print(f"Destination Port: {pkt[UDP].dport}")
-        else:
-            print(pkt.sprintf("%IP.proto%"))
         
         print("\n")
 
